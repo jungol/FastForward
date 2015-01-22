c2d5c79b64ea92996f5aa6e0a5bd251d74e57db1
Ethan Barhydt <ethanbarhydt@gmail.com>
2015-01-21 21:38:19 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sylaby::Application.config.secret_key_base = 'a8733377218dde8ba7897e5c2ff522c00c66e6229b692508a26f8d2748604cea394553919f303805f96f29ceb3a82bd28e226b3e3ad9d53e95e71310fa3a29a6'
