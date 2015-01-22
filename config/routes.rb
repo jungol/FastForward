2639f1a59c13bf469d23ef14b7587e751eee0be5
Ethan Barhydt <ethanbarhydt@gmail.com>
2015-01-21 23:05:51 -0500

    First list, oh. yeah.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Sylaby::Application.routes.draw do
-  get 'static_pages/home'
+  # get 'static_pages/home'
   root 'static_pages#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
