50583c1b3f1769c011f445d7cd2008401c12aaa4
Ethan Barhydt <ethanbarhydt@gmail.com>
2015-02-08 11:00:37 -0500

    Fix bug on adding user_items (wrong id)

--- app/controllers/user_items_controller.rb
+++ app/controllers/user_items_controller.rb
@@ -10,7 +10,7 @@ class UserItemsController < ApplicationController
 
   def destroy
     @item = Item.find(params[:user_item][:item_id])
-    current_user.user_items.find_by(@item.id).destroy
+    current_user.user_items.find_by(item_id: @item.id).destroy
     respond_to do |format|
       format.html { redirect_to root_path }
       format.js { @item }
