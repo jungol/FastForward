41cdfbb744be73fa3bec97a78eddc786e22b81ec
Ethan Barhydt <ethanbarhydt@gmail.com>
2015-03-24 07:41:53 -0400

    Experiment with filtering and pg

--- app/models/list.rb
+++ app/models/list.rb
@@ -17,6 +17,8 @@ class List < ActiveRecord::Base
   has_many :items, :through => :item_lists
   has_many :taggings
   has_many :tags, :through => :taggings
+  default_scope -> { where(id: "59") }
+
 
   # Creates hash of list_ids (keys) and tag counts (values)
   def self.hash_of_list_id_and(tags)
