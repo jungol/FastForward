8f776e16a1923216655d00d931a1932a854437c7
Ethan Barhydt <ethanbarhydt@gmail.com>
2015-04-15 19:00:33 -0400

    Set static_pages#home to root

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Sylaby::Application.routes.draw do
   resources :lists
   resources :collections
   resources :items, only: [:show]
-  root to: 'collections#index'
+  root to: 'static_pages#home'
   get 'static_pages/design'
   get 'static_pages/design2'
   get 'static_pages/design3'
