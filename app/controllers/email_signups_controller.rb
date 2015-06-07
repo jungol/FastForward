bbca4aad36df365e663dc0def2205113c45b59d3
Ethan Barhydt <ethanbarhydt@gmail.com>
2015-06-07 16:38:14 -0400

    Add email signups back into the site

--- app/controllers/email_signups_controller.rb
+++ app/controllers/email_signups_controller.rb
@@ -1,14 +1,17 @@
 class EmailSignupsController < ApplicationController
   def create
     @email_signup = EmailSignup.create(email_signup_params)
-    if @email_signup.save
-      flash[:success] = "Thanks for signing up!"
-    else
-      flash[:error] = @email_signup.errors.full_messages
+    # if @email_signup.save
+    #   flash[:success] = "Thanks for signing up!"
+    # else
+    #   flash[:error] = @email_signup.errors.full_messages
+    # end
+    @email_signup.save
+    respond_to do |format|
+      format.html
+      format.js
     end
-    redirect_to root_path
   end
-
   private
 
     def email_signup_params
--- app/views/email_signups/create.js.coffee
+++ app/views/email_signups/create.js.coffee
@@ -1,3 +1,3 @@
-$('.new_email_signup').html("Thanks for signing up!")
+$('.signup-form').html("Thanks for signing up!")
 
 
