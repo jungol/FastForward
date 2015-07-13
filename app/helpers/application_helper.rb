51a95b59208c1043db3e2a004982c853be703909
Ethan Barhydt <ethanbarhydt@gmail.com>
2015-07-12 22:35:02 -0400

    Change slogan a bit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   # Returns the full title on a per-page basis
   def full_title(page_title)
-    base_title = "DevEc GPS"
+    base_title = "EconForward"
     if page_title.empty?
       base_title
     else
