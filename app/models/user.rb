edc9e0679be8237a5b54f53ce708bfb0c4d45c16
Ethan Barhydt <ethanbarhydt@gmail.com>
2015-07-29 07:14:15 -0400

    Autocreate first collection for new users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  after_create :first_collection
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -10,6 +12,10 @@ class User < ActiveRecord::Base
     Item.all
   end
 
+  private
 
+    def first_collection
+      self.collections.create!(title:"My first collection")
+    end
 
 end
