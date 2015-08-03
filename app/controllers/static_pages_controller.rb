d88e577f66a5f425db040bfba9ffe9121b936588
Ethan Barhydt <ethanbarhydt@gmail.com>
2015-08-03 09:27:17 -0400

    Fix bug in date ordering

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
-    @feed_groups = Item.all.group_by(&:created_at_date).sort.reverse.paginate(page: params[:page], per_page: 2)
+    # @items_by_month = Item.all.group_by { |t| t.created_at.beginning_of_month}
+    @feed_groups = Item.all.group_by(&:created_at_date).sort_by { |date, items| Date.parse(date).beginning_of_month}
+    @feed_groups = @feed_groups.reverse.paginate(page: params[:page], per_page: 2)
   end
 end
