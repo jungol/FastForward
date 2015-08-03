b0dadf348caf0ae047dd4e3dc2252457e67c0907
Ethan Barhydt <ethanbarhydt@gmail.com>
2015-08-03 11:05:04 -0400

    Fix date ordering by month and day

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,9 @@
 class StaticPagesController < ApplicationController
   def home
     # @items_by_month = Item.all.group_by { |t| t.created_at.beginning_of_month}
-    @feed_groups = Item.all.group_by(&:created_at_date).sort_by { |date, items| Date.parse(date).beginning_of_month}
-    @feed_groups = @feed_groups.reverse.paginate(page: params[:page], per_page: 2)
+    @feed_groups = Item.all.group_by {|t| [t.created_at_date.to_date.beginning_of_month, t.created_at_date]}.sort.reverse
+    @feed_groups = @feed_groups.paginate(page: params[:page], per_page: 2)
   end
 end
+
+
