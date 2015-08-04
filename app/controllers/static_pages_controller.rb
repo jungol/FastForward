768b0f44f6c1eaafe4c290a255f76331c79add29
Ethan Barhydt <ethanbarhydt@gmail.com>
2015-08-04 11:24:31 -0400

    Change non-logged in homepage

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,7 +4,7 @@ class StaticPagesController < ApplicationController
     if user_signed_in?
       @items = Item.joins(:tags).where(tags: {id: current_user.tags}).all.uniq
     else
-      @items = Item.all
+      @items = Item.joins(:tags).where(tags: {id: User.find_by(email: "ethanbarhydt@gmail.com").tags}).all.uniq
     end
     @feed_groups = @items.group_by {|t| [t.created_at_date.to_date.beginning_of_month, t.created_at_date]}.sort.reverse
     @feed_groups = @feed_groups.paginate(page: params[:page], per_page: 2)
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150803180725) do
+ActiveRecord::Schema.define(version: 20150804144159) do
 
   create_table "collection_items", force: :cascade do |t|
     t.integer  "collection_id"
@@ -69,6 +69,8 @@ ActiveRecord::Schema.define(version: 20150803180725) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
